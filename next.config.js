63cc1381ac38f6c7fc0dfb67ebb2f73563c15272
Crawl <103999185+CrawlTheDev@users.noreply.github.com>
2022-05-26 08:59:34 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,5 +3,25 @@ module.exports = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true 
-  }
+  },
+  redirects() {
+		return [
+			{
+				source: '/github',
+				destination: 'https://github.com/ProxityStudios',
+				permanent: true
+			},
+			{
+				source: '/discord',
+				destination: 'https://discord.gg/wrhwwJQwas',
+				permanent: true
+			},
+                        {
+                               source: '/support-center'
+                               destination: 'https://discord.gg/wrhwwJQwas'
+                               permanent: true
+                        }
+		];
+	}
+
 }
